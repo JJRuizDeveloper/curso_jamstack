8ffb6b8c012a36de46e2c2b8333e79c6a0e6873d
JuanjoRuiz <juanjose.ruiz.munoz@gmail.com>
2022-09-19 17:40:58 +0200

    01 templates added

---
+++ sesion1/01_templates/.eleventy.js
@@ -0,0 +1,11 @@
+module.exports = config => {
+    return {
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dir: {
+            input: 'src',
+            output: 'dist'
+        }
+    };
+};
---
+++ sesion1/01_templates/src/index.md
@@ -0,0 +1,5 @@
+---
+greetings: Hola
+items: [{name: "Juanjo", surname: "Ruiz"}, {name: "Alfonso", surname: "Rodriguez"}]
+layout: 'layouts/home.html'
+---
